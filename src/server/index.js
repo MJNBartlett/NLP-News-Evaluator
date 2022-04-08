6a29b5327a318ccab4a64960bd1d6e72eb5a7803
MJNBartlett <100503137+MJNBartlett@users.noreply.github.com>
2022-04-08 17:18:33 +1000

    Added basic config for Dotenv

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,15 @@
-var path = require('path')
-const express = require('express')
+const path = require('path')
+require('dotenv').config()
 const mockAPIResponse = require('./mockAPI.js')
 
+const express = require('express')
 const app = express()
 
 app.use(express.static('dist'))
 
 console.log(__dirname)
+// Confirm env file is accessible.
+console.log(`Your API key is ${process.env.API_KEY}`);
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
