cf4a1fdb37277d5ad29810b45c722dd845349165
MJNBartlett <100503137+MJNBartlett@users.noreply.github.com>
2022-05-02 12:40:54 +1000

    Added initial functionality for handling API request and response.

--- src/server/index.js
+++ src/server/index.js
@@ -1,28 +1,67 @@
 // Initialises and configures dotenv for use of .env file.
 require('dotenv').config()
 
+const FormData = require('form-data'); //Required for FormData usage.
+const fetch = require('node-fetch') //Required for fetch request - node-fetch@2 reqiured version.
+
 const path = require('path')
 const mockAPIResponse = require('./mockAPI.js')
 
 const express = require('express')
 const app = express()
 
+
+//Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
 app.use(express.static('dist'))
 
-console.log(__dirname)
 // Confirm env file is accessible.
 console.log(`Your API key is ${process.env.API_KEY}`);
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    // res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
 
 app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
+  console.log("Recieved GET Request");
+  //Passing res variable in order to place res.send() method in async function.
+  getSentiment(res)
+  console.log("Ran function, sent response");
 })
+
+
+async function getSentiment(res){
+  const urlEntered = 'https://mjnbartlett.github.io/Blog-Project/coming-soon.html';
+  const formdata = new FormData();
+  formdata.append("key", process.env.API_KEY);
+  formdata.append("url", urlEntered);
+  formdata.append("lang", "en");
+
+  //Boilerplate options provided by MeaningCloud API
+  const requestOptions = {method: 'POST', body: formdata, redirect: 'follow'};
+  const response = await fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
+
+  try{
+    let status = response.status
+    let responseBody = await response.json()
+
+    console.log(status)
+    res.send(responseBody);
+
+  }catch(error){
+    console.log('error', error)
+  }
+}
