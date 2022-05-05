9faed24625d0ee20e27c063cfc080a17194473b5
MJNBartlett <100503137+MJNBartlett@users.noreply.github.com>
2022-05-05 19:46:31 +1000

    Adding return values for Jest testing.

--- src/client/js/updateUI.js
+++ src/client/js/updateUI.js
@@ -16,14 +16,18 @@ function updateUI(getData, formText){
     confidenceSpan.innerHTML = getData.confidence;
     scoreTagSpan.innerHTML = getData.score_tag;
     ironySpan.innerHTML = getData.irony;
+
+    return 'success'
   }else{
-    // Fallback for an Operation denied response from API due to invalid URL.
+    // Fallback for an 'Operation denied' response from API due to invalid URL.
     websiteSpan.innerHTML = `We're unable to reach '${formText}', please check the URL and try again.`;
     agreementSpan.innerHTML = '';
     subjectivitySpan.innerHTML = '';
     confidenceSpan.innerHTML = '';
     scoreTagSpan.innerHTML = '';
     ironySpan.innerHTML = '';
+
+    return 'fail'
   }
 }
 
